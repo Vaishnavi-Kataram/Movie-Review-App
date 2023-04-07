97fa3fc9d606746f34113cf701f378a8235db422
Vaishnavi Kataram <vaishnavi.kataram@gmail.com>
2023-04-07 00:23:40 -0400

    Created and styled the Carousel

--- movie-gold-v1/src/App.js
+++ movie-gold-v1/src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 
-  const[movies, setMovies] = useState();
+  const[movies, setMovies] = useState([]);
 
   const getMovies = async () =>{
 
     try{
-      const response = await api.get("/api/v1/movies");
+      const response = await api.get("http://localhost:8080/api/v1/movies/");
       console.log(response.data);
       setMovies(response.data);
+      
     }
     catch(err){
       console.log(err);
@@ -25,7 +29,14 @@ function App() {
 
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home movies={movies}/>}>
+
+          </Route>
+
+        </Route>
+      </Routes>
     </div>
   );
 }
