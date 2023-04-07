ccabac2dc79b570da6545b1343dcdc36e179ed44
Vaishnavi Kataram <vaishnavi.kataram@gmail.com>
2023-04-07 17:19:02 -0400

    updated code

--- movie-gold-v1/src/components/trailer/Trailer.js
+++ movie-gold-v1/src/components/trailer/Trailer.js
@@ -6,7 +6,7 @@ import React from 'react'
 
 const Trailer = () => {
 
-    let params = useParams();
+    let params = useParams(); 
     let key = params.ytTrailerId;
 
   return (
--- movies/src/main/java/dev/vaishnavi/movies/MovieController.java
+++ movies/src/main/java/dev/vaishnavi/movies/MovieController.java
@@ -4,14 +4,12 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController // this is our first official rest API controller
 @RequestMapping("/api/v1/movies") // instead of mapping to localhost:8080, map it to localhost:8080/api/v1/movies
 public class MovieController {
